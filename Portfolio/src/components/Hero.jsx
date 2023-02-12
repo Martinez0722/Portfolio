a37a3cae064a6fbc4ced11cbbf8ba11060fd1c60
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-02-12 11:59:31 -0300

    skill

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import hero from "../assets/images/hero.png";
+import { Link } from "react-router-dom"
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const social_media = [
+    {
+      name: "logo-github", 
+      live_link:"https://github.com/Martinez0722" ,
+    },
+    {
+      name:"logo-linkedin",
+      live_link:"https://www.linkedin.com/in/rafaelfelipetech/"
+    }
+  ];
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex py-10 md:flex-row flex-col items-center"
+    >
+      <div className="flex-1 flex items-center justify-center h-full">
+        <img src={hero} alt="" className="md:w-11/12 h-full object-cover" />
+      </div>
+      <div className="flex-1">
+        <div className="md:text-left text-center">
+          <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+            <span className="text-cyan-600 md:text-6xl text-5xl">
+              Olá!
+              <br />
+            </span>
+            Eu sou <span>Rafael</span>
+          </h1>
+          <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
+            Dev Fullstack
+          </h4>
+          <button className="btn-primary mt-8">Entre em contato</button>
+          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+            {social_media?.map((icon) => (
+              <div
+                key={icon}
+                className="text-gray-600 hover:text-white cursor-pointer "
+              >
+                <a href={icon.live_link} target="_blank">
+                  <ion-icon name={icon.name}>
+                  </ion-icon>
+                </a> 
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- Portfolio/src/components/Skills.jsx
+++ Portfolio/src/components/Skills.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  const skills = [ 
+    {
+      logo: 'logo-html5', level:'Advanced', count: 86
+    },
+    {
+      logo: 'logo-css3', level:'Advanced', count: 86
+    },
+    {
+      logo: 'logo-javascript', level:'Advanced', count: 86
+    },
+    {
+      logo: 'logo-react', level:'Advanced', count: 86
+    },
+    {
+      logo: 'logo-nodejs', level:'intermediate', count: 80
+    },
+    {
+      logo: 'logo-python', level:'Advanced', count: 80
+    },
+  ]
+  return (
+  <section id="skills" className="py-10 bg-gray-800 relative">
+    <div className="mt-8 text-gray-100 text-center">
+      <h3>Minhas <span className="text-cyan-600">Habilidades</span></h3>
+      <p className="text-gray-400 mt-3 text-lg">Meus conhecimentos</p>
+      <div className="flex items-center justify-center mt-12 gap-2 flex-wrap">
+        {
+          skills?.map((skill, i)=>(
+              <div key={i} className="border-2 border-cyan-600 relative min-w-[10rem]
+                max-w[16rem] bg-gray-900 p-10 rounded-xl">
+                  <div 
+                    style={{
+                    background:`conic-gradient(rgb(8,146,170) ${skill.count}%, ${skill.count}%)`,
+                    }} 
+                    className="w-32 h-32 flex items-center justify-center 
+                    rounded-full">
+                      <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex
+                        items-center justify-center">
+                          <ion-icon name={skill.logo}></ion-icon>
+                      </div>
+                  </div>
+                <p className="text-xl mt-3">{skill.level}</p>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  </section>
+  )
 };
 
 export default Skills;
